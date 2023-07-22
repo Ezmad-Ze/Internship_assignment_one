42b18dbf2540830083e4d68b53a998f2852228a8
Ezmad-Ze <90828396+Ezmad-Ze@users.noreply.github.com>
2023-07-22 22:39:24 +0300

    creating navbar

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,36 @@
-console.log('HELLO');
+const hamburger = document.querySelector(".header__ham");
+const close = document.querySelector(".header__close");
+const nav = document.querySelector(".header__nav");
+const mediaQuery = window.matchMedia("(max-width: 640px)");
 
-const test = () => {
-	console.log('this is a test');
-};
+let value = true;
+
+hamburger.addEventListener("click", () => {
+  value == true ? check(mediaQuery) : null;
+});
+
+close.addEventListener("click", () => {
+  value == false ? check(mediaQuery) : null;
+});
+
+function check(mediaQuery) {
+  if (mediaQuery.matches) {
+    if (value == true) {
+      nav.style.display = "block";
+      close.style.display = "block";
+      hamburger.style.display = "none";
+      value = false;
+    } else {
+      nav.style.display = "none";
+      hamburger.style.display = "block";
+      close.style.display = "none";
+      value = true;
+    }
+  } else {
+    nav.style.display = "flex";
+    close.style.display = "none";
+    hamburger.style.display = "none";
+    value = false;
+  }
+}
+mediaQuery.addEventListener("change", check);
